30b2b1a24ab924ea4dd85226b4f7a6ee62b92aef
Andriawan Firmansyah <andriawnfirmansyah@gmail.com>
2023-12-30 21:11:21 +0700

    feat: categories blog

--- src/components/ui/Categories.jsx
+++ src/components/ui/Categories.jsx
@@ -3,9 +3,9 @@ function Categories() {
   return (
     <div className="flex gap-10 font-nunito font-semibold text-base">
         <button className="text-primary underline underline-offset-8 cursor-pointer">Web Design</button>
-        <button className="cursor-pointer text-black hover:text-primary">Business</button>
-        <button className="cursor-pointer text-black hover:text-primary">Industy</button>
-        <button className="cursor-pointer text-black hover:text-primary">Technology</button>
+        <button className="cursor-pointer hover:text-primary">Business</button>
+        <button className="cursor-pointer hover:text-primary">Industy</button>
+        <button className="cursor-pointer hover:text-primary">Technology</button>
     </div>
   )
 }
