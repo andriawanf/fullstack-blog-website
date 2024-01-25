d14e9df3d99832536415a6a4ce2ac09bd1b8f5b0
Andriawan Firmansyah <andriawnfirmansyah@gmail.com>
2024-01-25 13:53:55 +0700

    feat(page):create page for blog view

--- src/components/allBlogs/BlogsGrid.jsx
+++ src/components/allBlogs/BlogsGrid.jsx
@@ -1,9 +1,28 @@
 import { useContext } from "react";
 import { ThemeContext } from "../../contexts/ThemeContext";
 import Button from "../ui/Button";
+import { useEffect } from "react";
+import axios from "axios";
+import { useState } from "react";
+import { format } from 'date-fns';
+import { Link } from "react-router-dom";
 
 function BlogsGrid() {
-    const {theme} = useContext(ThemeContext);
+    const [dataPosts, setDataPosts] = useState([]);
+    const { theme } = useContext(ThemeContext);
+
+    useEffect(() => {
+        const fetchPost = async () => {
+            try {
+                const respones = await axios.get('/api/post/getPosts');
+                const dataPost = respones.data.posts.slice(0,1);
+                setDataPosts(dataPost);
+            } catch (error) {
+                console.log(error.message);
+            }
+        }
+        fetchPost();
+    }, [dataPosts]);
     return (
         <section className="w-full overflow-hidden">
             <div className="relative">
@@ -17,24 +36,26 @@ function BlogsGrid() {
                     </div>
                 </div>
                 <div className="flex flex-wrap px-4 -mx-4 my-18">
-                    <div className="w-full mb-12 lg:mb-0 lg:w-1/2">
-                        <a className="block w-full group" href="#">
-                            <img
-                                className="block object-cover w-full mb-5 rounded-3xl"
-                                src="https://plus.unsplash.com/premium_photo-1663852297654-56d979cf72d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8YmxvZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
-                                alt=""
-                            />
-                            <span className="block mb-5 text-primaryContent/60 font-nunito">Jul 20, 2023</span>
-                            <h4 className="mb-5 text-3xl font-semibold font-dm">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                            </h4>
-                            <p className={theme === 'bg-primaryBackground text-primaryContent' ? "max-w-xl text-lg text-primaryContent/60 font-nunito" : "max-w-xl text-lg text-primaryBackground/60 font-nunito"}>
-                                Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
-                                officia consequat duis enim velit mollit xercitation veniam consequat sunt
-                                nostrud amet.
-                            </p>
-                        </a>
-                    </div>
+                    {
+                        dataPosts.map((post) => 
+                            <div className="w-full mb-12 lg:mb-0 lg:w-1/2" key={post.id}>
+                                <Link to={`/blog/${post.slug}`} className="block w-full group" >
+                                    <img
+                                        className="block object-cover w-full mb-5 rounded-3xl"
+                                        src={post.imageContent}
+                                        alt=""
+                                    />
+                                    <span className="block mb-5 text-primaryContent/60 font-nunito">{format(new Date(post.createdAt), "do MMM yyyy")}</span>
+                                    <h4 className="mb-5 text-3xl font-semibold font-dm">
+                                        {post.title}
+                                    </h4>
+                                    <div className={theme === 'bg-primaryBackground text-primaryContent' ? "max-w-xl text-lg text-primaryContent/60 font-nunito line-clamp-3 post-content" : "max-w-xl text-lg text-primaryBackground/60 font-nunito line-clamp-3"} dangerouslySetInnerHTML={{__html: post && post.content}}>
+                                        
+                                    </div>
+                                </Link>
+                            </div>
+                        )
+                    }
                     <div className="w-full pl-6 lg:w-1/2">
                         <a className="mb-6 group md:flex" href="#">
                             <img
---
+++ src/pages/BlogPage.jsx
@@ -0,0 +1,120 @@
+import React from 'react'
+import PageContainer from '../layouts/PageContainer'
+import Navbar from '../layouts/Navbar'
+import Footers from '../components/footer/Footers'
+import { useParams } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios';
+import { useEffect } from 'react';
+
+function BlogPage() {
+    const { blogSlug } = useParams();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const [post, setPost] = useState([]);
+    const [recentPosts, setRecentPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPost = async () => {
+            try {
+                setLoading(true);
+                const res = await axios.get(`/api/post/getposts?slug=${blogSlug}`)
+                    .then(data => setPost(data.data.posts[0]));
+                if (!res.ok) {
+                    setError(true);
+                    setLoading(false);
+                    return;
+                }
+                if (res.ok) {
+                    setLoading(false);
+                    setError(false);
+                }
+            } catch (error) {
+                setError(true);
+                setLoading(false);
+            }
+        };
+        fetchPost();
+    }, [blogSlug]);
+
+    useEffect(() => {
+        try {
+            const fetchRecentPosts = async () => {
+                const res = await axios.get(`/api/post/getposts?limit=3`)
+                    .then(data => setRecentPosts(data.data.posts));
+                // if (res.ok) {
+                //     setRecentPosts(data.data.posts);
+                // }
+            };
+            fetchRecentPosts();
+        } catch (error) {
+            console.log(error.message);
+        }
+    }, []);
+
+    console.log(recentPosts)
+    return (
+        <PageContainer>
+            <Navbar />
+            <main className="mt-10">
+                <div className="relative w-full mx-auto mb-4 md:mb-0">
+                    <div className="flex flex-col items-center justify-center w-full px-4 lg:px-0 ">
+                        <a
+                            href="#"
+                            className="inline-flex items-center justify-center mb-3 text-primaryDark font-dm"
+                        >
+                            {post && post.category}
+                        </a>
+                        <h2 className="max-w-5xl mb-12 text-4xl font-semibold leading-tight text-center font-dm">
+                            {post && post.title}
+                        </h2>
+                    </div>
+
+                    <img src={post && post.imageContent} className="object-cover w-full lg:rounded-2xl h-80" />
+                </div>
+
+                <div className="flex flex-col font-medium lg:flex-row lg:space-x-12 font-nunito">
+
+                    <div className="w-full px-4 mt-12 text-lg leading-relaxed lg:px-0 lg:w-3/4">
+                        <p className="pb-6 text-justify">
+                            {post && post.description}
+                        </p>
+
+                        <div className="pb-6 text-justify post-content" dangerouslySetInnerHTML={{ __html: post && post.content }}></div>
+                    </div>
+
+                    <div className="w-full max-w-screen-sm m-auto mt-12 lg:w-1/4">
+                        <div className="p-4 border-t border-b md:border md:rounded">
+                            <div className="flex py-2">
+                                <img src="https://randomuser.me/api/portraits/men/97.jpg"
+                                    className="object-cover w-10 h-10 mr-2 rounded-full" />
+                                <div>
+                                    <p className="text-sm font-semibold text-gray-700"> Mike Sullivan </p>
+                                    <p className="text-xs font-semibold text-gray-600"> Editor </p>
+                                </div>
+                            </div>
+                            <p className="py-3 text-gray-700">
+                                Mike writes about technology
+                                Yourself required no at thoughts delicate landlord it be. Branched dashwood do is whatever it.
+                            </p>
+                            <button className="flex items-center justify-center w-full px-2 py-1 text-gray-100 bg-green-700 rounded">
+                                Follow
+                                <i className='ml-2 bx bx-user-plus' ></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className='flex flex-col items-center justify-center mb-5'>
+                    <h1 className='mt-5 text-xl'>Recent articles</h1>
+                    <div className='flex flex-wrap justify-center gap-5 mt-5'>
+                        {/* {recentPosts &&
+                            recentPosts.map((post) => <PostCard key={post._id} post={post} />)} */}
+                    </div>
+                </div>
+            </main>
+            <Footers />
+        </PageContainer>
+    )
+}
+
+export default BlogPage
